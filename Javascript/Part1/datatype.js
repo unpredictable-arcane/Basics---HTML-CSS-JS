5427ca08a441908cdd8161383e55f53c89512ead
unpredictable-arcane <arcane.anuj@gmail.com>
2026-01-13 15:15:16 +0530

    added datatype.js

---
+++ Javascript/Part1/datatype.js
@@ -0,0 +1,30 @@
+console.log("hello");
+// console.log("chai");
+
+/*
+String
+Number
+Boolean
+BigInt
+
+undefined
+null
+
+Object
+
+Symbol
+*/
+
+// var score = 197
+
+let score = 103;
+let name = "kaka.da.dhaba";
+let isLoggedin = false;
+
+// Object
+let teaTypes = ["lemon tea", "orange tea", "oolong tea"];
+let user = {firstname: "Anuj", lastname: "mishra"};
+
+let getScore = score;
+
+console.log(getScore);
