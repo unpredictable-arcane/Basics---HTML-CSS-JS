7ce3aaf55fe646d6c5ad309b3b3c5343bc077c79
unpredictable-arcane <arcane.anuj@gmail.com>
2026-01-13 15:16:58 +0530

    added operations.js

---
+++ Javascript/Part1/operations.js
@@ -0,0 +1,29 @@
+let score = 39
+let bonus = 24
+
+let totalScore = score + bonus 
+
+
+let addition = 7 + 7 
+let subtract = 44 - 8
+let divi = 77 / 7 
+let mult = 7 * 4
+let reminder = 9 % 2
+let expo = 3 ** 7
+
+let myscore = 110;
+myscore++; 
+
+let credits = 54; 
+credits--; 
+
+//Comparison operation  
+
+let num1 = 3 
+let num2 = 5 
+let num3 = 5 
+
+console.log(num3 == num2); // true  
+console.log(num1 != num3); //true 
+console.log(num2 > num3); //false 
+console.log(num1 > num3); //false 
