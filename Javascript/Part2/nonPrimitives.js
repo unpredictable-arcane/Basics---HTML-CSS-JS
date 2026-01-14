09e477c9d2b89a364bacb39069c8c36ecc485920
unpredictable-arcane <arcane.anuj@gmail.com>
2026-01-14 20:35:58 +0530

    added nonPrimitives.js

---
+++ Javascript/Part2/nonPrimitives.js
@@ -0,0 +1,25 @@
+const username = {
+    firstname: "Anuj",
+    isLoggedin: true,
+};
+
+username.firstname = "Anuj";
+username.lastname = "Mishra";
+
+// console.log(username.firstname);
+// console.log(username.lastname);
+// console.log(username);
+// console.log(typeof username);
+
+let today = new Date();
+// console.log(today.getDate());
+
+// Array 
+
+let anotherUser = ["Anuj", true];
+
+console.log(anotherUser[0]);
+
+let isValue = "2abc";
+console.log(typeof Number(isValue));
+console.log(Number(null));
