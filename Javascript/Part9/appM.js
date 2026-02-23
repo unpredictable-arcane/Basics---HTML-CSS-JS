4ed19680af587d0e240a1fd6d1118a3738f10fce
unpredictable-arcane <arcane.anuj@gmail.com>
2026-02-23 17:33:00 +0530

    Add Javascript Part 9 appM file

---
+++ Javascript/Part9/appM.js
@@ -0,0 +1,11 @@
+// Module import file 
+
+//default import 
+import multiply from "./mathOpeM";
+
+//named import 
+import { add, subtract } from "./mathOpeM.js";
+
+console.log(multiply(2, 2));
+console.log(add(2, 2));
+
