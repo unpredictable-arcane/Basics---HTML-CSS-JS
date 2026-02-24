6cec1617ea3d666db4ebc310f9f6397cc2bf451b
unpredictable-arcane <arcane.anuj@gmail.com>
2026-02-24 12:30:46 +0530

    Add Javascript Part 9 asyncAwait file

---
+++ Javascript/Part9/asyncAwait.js
@@ -0,0 +1,22 @@
+function fetchUserData() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve({ name: "chaicode", url: "https://chaicode.com" });
+    }, 3000);
+  });
+}
+
+async function getUserData() {
+    try {
+        console.log("Fetching user data...");
+        const userData = await fetchUserData();
+        console.log("User data fetched successfully");
+        
+        console.log("User data: ", userData);
+    } catch (error) {
+      console.log("Error fetching data", error);
+      
+    }
+}
+
+getUserData();
