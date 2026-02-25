2ee364cfb0e1ecf41ec95714980bdd3bf8a9d416
unpredictable-arcane <arcane.anuj@gmail.com>
2026-02-26 04:35:35 +0530

    Add Javascript Part 9 explore file

---
+++ Javascript/Part9/explore1.js
@@ -0,0 +1,13 @@
+function sayLoveYou() {
+    console.log("I would like to say love u niks");
+}
+
+setTimeout(() => {
+    sayLoveYou();
+}, 4000);
+
+console.log("chaicode");
+
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+}
